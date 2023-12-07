0484d388e079c1313345d55bb065b64a7ad6a676
quanxily <yangquanxi80@gmail.com>
2023-12-07 09:49:12 +0800

    make it better

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    x ="作者:楊荃喜11/26<br>"
+    x ="作者:楊荃喜12/7<br>"
     x +="<a href=/mis>資管導論</a><br>"
     x +="<a href=/today>日期時間</a><br>"
     x +="<a href=/about>荃喜網頁</a><br>"
@@ -25,6 +25,7 @@ def index():
     x +="<a href=/spider>請爬取子青老師的課程名稱、網址及圖片</a><br>"
     x +="<a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
     x +="<a href=/searchQ>輸入關鍵字進行資料查詢</a><br>"
+    x+= "<br><a href=/movie_rate>讀取開眼電影即將上映影片(含分級資訊)，寫入Firestore</a><br>"
     return x
 
 @app.route("/mis")
@@ -149,6 +150,62 @@ def movie():
       doc_ref = db.collection("電影").document(movie_id)
       doc_ref.set(doc)
     return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate 
+
+@app.route("/movie_rate")
+def movie_rate():
+    url = "http://www.atmovies.com.tw/movie/next/"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".filmListAllX li")
+    lastUpdate = sp.find(class_="smaller09").text[5:]
+
+    for x in result:
+        picture = x.find("img").get("src").replace(" ", "")
+        title = x.find("img").get("alt")    
+        movie_id = x.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+        hyperlink = "http://www.atmovies.com.tw" + x.find("a").get("href")
+
+        t = x.find(class_="runtime").text
+        showDate = t[5:15]
+
+        showLength = ""
+        if "片長" in t:
+            t1 = t.find("片長")
+            t2 = t.find("分")
+            showLength = t[t1+3:t2]
+
+        r = x.find(class_="runtime").find("img")
+        rate = ""
+        if r != None:
+            rr = r.get("src").replace("/images/cer_", "").replace(".gif", "")
+            if rr == "G":
+                rate = "普遍級"
+            elif rr == "P":
+                rate = "保護級"
+            elif rr == "F2":
+                rate = "輔12級"
+            elif rr == "F5":
+                rate = "輔15級"
+            else:
+                rate = "限制級"
+
+        doc = {
+            "title": title,
+            "picture": picture,
+            "hyperlink": hyperlink,
+            "showDate": showDate,
+            "showLength": showLength,
+            "rate": rate,
+            "lastUpdate": lastUpdate
+        }
+
+        db = firestore.client()
+        doc_ref = db.collection("電影含分級").document(movie_id)
+        doc_ref.set(doc)
+    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
+
+
 @app.route("/searchQ", methods=["POST","GET"])
 def searchQ():
     if request.method == "POST":
@@ -167,6 +224,6 @@ def searchQ():
     else:  
         return render_template("input.html")
     
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+    #app.run()
 
