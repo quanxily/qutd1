1b11fb5735cdb044c940bfb714526b9739677956
quanxily <yangquanxi80@gmail.com>
2023-12-07 11:49:24 +0800

    make it better

--- index.py
+++ index.py
@@ -212,11 +212,8 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req.get("queryResult").get("action")
-    #msg =  req.get("queryResult").get("queryText")
-    #info = "動作：" + action + "； 查詢內容：" + msg
-    if (action == "rateChoice"):
-        rate =  req.get("queryResult").get("parameters").get("rate")
-        info = "您選擇的電影分級是：" + rate
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
      return make_response(jsonify({"fulfillmentText": info}))
 
 
